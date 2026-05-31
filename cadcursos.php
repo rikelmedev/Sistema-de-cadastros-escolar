f4145dcf8da48f432b9b6ad20128726be1174b2f
Rikelme <rikelmemini100@gmail.com>
2026-05-30 23:58:00 -0300

    Correção do cadcursos.php

--- cadcursos.php
+++ cadcursos.php
@@ -1,32 +1,49 @@
 ﻿<html>
 <head>
-<title></title>
+     <title>Cadastro de Cursos</title>
 </head>
 <body>
+
 <?php
+
 include_once ('conexao.php');
-//recupernado
+
+//RECUPERAÇÃO DOS DADOS DO FORMULÁRIO
 $codigo = $_POST['codcurso'];
 $nome = $_POST['nomecurso'];
 $cod1 = $_POST['coddisciplina1'];
 $cod2 = $_POST['coddisciplina2'];
 $cod3 = $_POST['coddisciplina3'];
+
+
 //criando linha de insert
 $sqlinsert = "insert into curso(codcurso, nome, coddisciplina01, coddisciplina02, coddisciplina03)
 VALUES ('$codigo', '$nome', '$cod1', '$cod2', '$cod3')";
-//executando instrucao sql
+
+
+//EXECUÇÃO E VERIFICAÇÃO DO RESULTADO
 $resultado = @mysqli_query($conexao, $sqlinsert);
-if (!$resultado)
-{
-die('Query Inválida: '. @mysqli_error($conexao));
-}else
-{ 
-echo "registro realizado com sucesso!";
-}
-mysqli_close($conexao);
+
+if (!$resultado){
+
+if (@mysqli_error($conexao) == 1062){
+   echo "<p>Código <strong>$codigo</strong> já esta cadastrado.
+   Use um codigo de curso diferente.</p>";
+   
+   } else { 
+   echo "<p>Erro ao realizar o cadastro. Tente novamente.</p>";
+   }
+   
+   } else { 
+   echo "<p>Curso <strong>$nomecurso</strong> cadastrado com sucesso!</p>";
+   }
+
+   mysqli_close($conexao);
 ?>
+
 <br>
 <br>
-<input type='button' onclick="window.location='index.php';" VALUE ="Voltar">
+<input type='button' onclick="window.location='index.php';" VALUE ="Voltar ao Menu">
+
 </body>
 </html>
